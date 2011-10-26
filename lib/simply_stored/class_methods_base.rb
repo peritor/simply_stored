31a789c90eb9399d5d0470f1a6943d249f788162
Michael Solovyov <spinalfluid@gmail.com>
2011-10-25 17:18:22 -0700

    Rescue symptom of hasbleongs to class name error

--- lib/simply_stored/class_methods_base.rb
+++ lib/simply_stored/class_methods_base.rb
@@ -2,7 +2,12 @@ module SimplyStored
   module ClassMethods
     module Base
       def get_class_from_name(klass_name)
+      
         klass_name.to_s.gsub('__','/').classify.constantize
+        
+        rescue NameError
+          klass_name.constantize
+        
       end
       
       def foreign_key
